0f660eae97acb2c2c4f35cf8885fe8d23d376355
Arnaud DRAZEK <adrazek@gmail.com>
2017-05-09 14:56:50 +0000

    Fixing moodle api url (ssl enabled)

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,6 @@
 var UsersApi = function() {
   var searchFor = function(query, callback) {
-    fetch("http://open-business-labs.herokuapp.com/v1/users/" + query, {
+    fetch("https://open-business-labs.herokuapp.com/v1/users/" + query, {
       method: 'get',
       headers: {
         'Authorization': 'bearer 11dec774b37a3dd9e0d26e1a57796f1d46cbf71a74146bdd5a90b11ffe2abfb2',
