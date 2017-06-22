197519f4d3b62249e894f8802f914d453308f881
Arnaud DRAZEK <adrazek@gmail.com>
2017-06-22 13:53:12 +0000

    Fixing production env

--- scripts/prod.js
+++ scripts/prod.js
@@ -1,28 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const openamAgent = require('openam-agent');
-const agentConfig = require('../config/openam.json');
-const agent = openamAgent(agentConfig);
-const PORT = process.env.PORT || 3000;
-
-var cookieShield = new openamAgent.CookieShield({cdsso: true});
-
-var router = express.Router();
-router.use(agent.shield(cookieShield));
-//app.use('*', router);
-app.use(agent.cdsso());
-
-app.get("/agent/cdsso", function(request, response) {
-  response.send("CDSSO AGENT");
-});
-app.get("/", function(request, response) {
-  response.sendFile(path.resolve('www/index.html'));
-});
+const PORT = process.env.PORT || 3001;
 
 app.use("/", express.static(path.resolve('www')));
 
 app.get("*", function(request, response) {
+console.log("UH?")
   response.sendFile(path.resolve('www/index.html'));
 });
 
