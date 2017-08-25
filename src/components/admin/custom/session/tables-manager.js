74ebf65d77583c8ac1d7fcf625a5b908cc2e1f1a
Arnaud DRAZEK <adrazek@gmail.com>
2017-08-25 14:11:28 +0000

    Disable users managment when session has been launched

--- src/components/admin/custom/session/tables-manager.js
+++ src/components/admin/custom/session/tables-manager.js
@@ -9,12 +9,14 @@ class TablesManager extends React.Component {
   render() {
     return (
       <div>
-        <div className="session-player-form-container">
-          <Players session={this.props.entity} />
-        </div>
-        <div className="session-table-form-container">
-          <Tables session={this.props.entity} players={this.getAvailablePlayers()} />
-        </div>
+        {this.props.entity.playable === "to_launch" || (this.props.entity.playable === "pause" && this.props.entity.rounds.length == 1 && this.props.entity.current_step == "waiting_players")
+        ? [<div className="session-player-form-container">
+             <Players session={this.props.entity} />
+            </div>,
+            <div className="session-table-form-container">
+              <Tables session={this.props.entity} players={this.getAvailablePlayers()} />
+            </div>]
+        : <span>Tu ne peux pas modifier les joueurs d'une session qui a déjà été lancée</span>}
       </div>
     )
   }
