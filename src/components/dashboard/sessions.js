b3399957beda55d7691fc503a2cc8b93c564c68e
Arnaud DRAZEK <adrazek@gmail.com>
2017-08-29 13:23:12 +0000

    Managing sessions in dashboard for admins

--- src/components/dashboard/sessions.js
+++ src/components/dashboard/sessions.js
@@ -14,6 +14,7 @@ class Sessions extends React.Component {
   }
 
   render() {
+    var sessions = this.buildSessions()
     return (
       <section className="content">
         <Grid fluid>
@@ -22,18 +23,29 @@ class Sessions extends React.Component {
               <h1><i className={"pe pe-7s-joy text-warning"}></i> Jeux</h1>
             </Col>
          </Row>
-         {this.props.sessions.map(session => {
-           return <Session key={"session-" + session.id} session={session} />
-         })}
+         {sessions.filter(session => {return session !== null }).length > 0
+          ? sessions
+          : <Row><Col xs={12}>Vous n'avez été invité à aucune session pour le moment</Col></Row>}
        </Grid>
       </section>
     )
+  }
+
+  buildSessions() {
+    return this.props.sessions.map(session => {
+             var ui = <Session key={"session-" + session.id} session={session} />
+             if (this.props.me.role === "admin" && session.players.filter(player => {player.id === this.props.me.id}).length === 0) {
+               ui = null
+             }
+             return ui
+           })
   }
 
 }
 
 function mapStateToProps(state) {
   return {
+    me: state.userState.me || null,
     sessions: state.sessionState.sessions || []
   }
 }
