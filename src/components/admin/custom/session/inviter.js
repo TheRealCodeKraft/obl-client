62f92616707c85b09e36527f7cc1beb0b36cae39
Arnaud DRAZEK <adrazek@gmail.com>
2017-08-29 15:38:40 +0000

    Managing invitations

--- src/components/admin/custom/session/inviter.js
+++ src/components/admin/custom/session/inviter.js
@@ -4,6 +4,8 @@ import SessionClient from 'clients/session'
 
 import { Grid, Row, Col, Table, Button } from 'react-bootstrap'
 
+import Moment from 'react-moment'
+
 class Inviter extends React.Component {
 
   render() {
@@ -15,28 +17,45 @@ class Inviter extends React.Component {
               <thead>
                 <tr>
                   <th>Nom</th>
+                  <th>Dernier envoi</th>
                   <th></th>
                 </tr>
               </thead>
               <tbody>
-                {this.props.entity.players.map(player => {
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td><Button onClick={this.sendInvitationToAll.bind(this)}>Inviter tout le monde</Button></td>
+                </tr>
+                {this.getPlayers().map(player => {
                   return (
                     <tr>
                       <td>{player.fullname}</td>
+                      <td><Moment format="DD/MM/YYYY HH:mm:ss">{this.getLastUpdate(player)}</Moment></td>
                       <td><Button onClick={this.sendInvitation.bind(this, player)}>Inviter</Button></td>
                     </tr>
                   )
                 })}
-                <tr>
-                  <td></td>
-                  <td><Button onClick={this.sendInvitationToAll.bind(this)}>Inviter tout le monde</Button></td>
-                </tr>
               </tbody>
             </Table>
           </Col>
         </Row>
       </Grid>
     )
+  }
+
+  getPlayers() {
+    return this.props.entity.players.sort(function(a, b) {
+      if (a.firstname === null) return 1
+      if (b.firstname === null) return -1
+      return a.firstname > b.firstname
+    })
+  }
+
+  getLastUpdate(player) {
+    var filter = this.props.entity.invitations.filter(invit => { return invit.user_id == player.id })
+    if (filter.length === 1) return filter[0].updated_at
+    else return null
   }
 
   sendInvitation(player) {
