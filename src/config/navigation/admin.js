c3885a901f33fdd5cecca2c68bd8c6cf080340eb
nO <arnaud@codekraft.fr>
2018-01-16 16:48:14 +0100

    Trying to fix icon set

--- src/config/navigation/admin.js
+++ src/config/navigation/admin.js
@@ -24,12 +24,14 @@ const config = {
   wrapper: {
     component: MainWrapper
   },
+  /*
   iconSet: "pe pe-7s-",
   icons: {
     delete: "junk",
     edit: "pen",
     view: "look"
   },
+  */
   menu: {
     general: {
       navLink: Link,
